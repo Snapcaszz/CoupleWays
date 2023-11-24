49871d1c39dd3f6c7a1cdd9d7f24c2657fd7701f
Matheus Singh Cardoso <matheus.scep@gmail.com>
2023-11-24 13:48:52 -0300

    IMplemented dark_mode

--- couple_ways/routes.py
+++ couple_ways/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, url_for, request
+from flask import Blueprint, render_template, url_for, request, session, redirect
 
 
 pages = Blueprint(
@@ -12,3 +12,13 @@ def landing_page():
         "landing_page.html",
         title="Couple Ways",
     )
+
+@pages.route("/toogle-theme")
+def toggle_theme():
+    current_theme = session.get("theme")
+    if current_theme == "dark":
+        session["theme"] = "light"
+    else: 
+        session["theme"] = "dark"
+        
+    return redirect(request.args.get("current_page"))
