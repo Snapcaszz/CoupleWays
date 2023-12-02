94d544e4723b052929d63ae6585a66693bdcbd7f
Matheus Singh Cardoso <matheus.scep@gmail.com>
2023-12-02 02:07:02 -0300

    set the default theme to dark

--- couple_ways/__init__.py
+++ couple_ways/__init__.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask
+from flask import Flask, session
+from datetime import timedelta
 from dotenv import load_dotenv
 from pymongo import MongoClient
 from couple_ways.routes import pages
@@ -15,10 +16,18 @@ def create_app():
         "SECRET_KEY", "5B2A8DB6FB704A5A90E09E2DF2EE056F45E741CCD005644389D276C29A94F69D"
     )
     app.config["UPLOADED_PHOTOS_DEST"] = "./couple_ways/static/uploads"
-    
+
     configure_uploads(app, photos)
 
     app.db = MongoClient(app.config["MONGODB_URI"]).get_default_database()
 
     app.register_blueprint(pages)
+
+    @app.before_request
+    def set_default_theme():
+        # Check if the "theme" key is not already in the session
+        if "theme" not in session:
+            session["theme"] = "dark"
+
     return app
+
